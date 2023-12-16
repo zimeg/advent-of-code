523db6402cbc97750465c4405a70f5d64c222660
Ethan Zimbelman <ethan.zimbelman@me.com>
2023-12-15 23:55:54 -0800

    feat(2023.5.2): iterate over large ranges of seeds to find the lowest

--- v2023/src/main.rs
+++ v2023/src/main.rs
@@ -26,6 +26,7 @@ fn main() {
         "4.1" => stars::day_04::scratchcard_points(input),
         "4.2" => stars::day_04::scratchcard_replay(input),
         "5.1" => stars::day_05::seed_to_location_min(input),
+        "5.2" => stars::day_05::seed_to_location_range(input),
         _ => {
             eprintln!("error: star '{}' does not yet shine", args.star);
             std::process::exit(1);
--- v2023/src/stars/day_05.rs
+++ v2023/src/stars/day_05.rs
@@ -1,4 +1,5 @@
 use crate::gifts::parser;
+use std::str::Split;
 
 struct Mapping {
     destination: u64,
@@ -9,6 +10,33 @@ struct Mapping {
 pub fn seed_to_location_min(input: String) -> u32 {
     let mut mappings = input.split("\n\n");
     let seeds = parser::gather_numbers(mappings.next().unwrap());
+    let maps = create_mappings(mappings);
+    let mut lowest = u64::MAX;
+    for seed in seeds {
+        let location = permute_location(seed.into(), &maps);
+        lowest = std::cmp::min(lowest, location);
+    }
+    lowest.try_into().unwrap()
+}
+
+pub fn seed_to_location_range(input: String) -> u32 {
+    let mut mappings = input.split("\n\n");
+    let ranges = parser::gather_numbers(mappings.next().unwrap());
+    let maps = create_mappings(mappings);
+    let mut lowest = u64::MAX;
+    for (index, start) in ranges.clone().into_iter().enumerate() {
+        if index % 2 != 0 {
+            continue;
+        }
+        for seed in start..start + ranges.get(index + 1).unwrap() {
+            let location = permute_location(seed.into(), &maps);
+            lowest = std::cmp::min(lowest, location);
+        }
+    }
+    lowest.try_into().unwrap()
+}
+
+fn create_mappings(mappings: Split<&str>) -> Vec<Vec<Mapping>> {
     let mut maps: Vec<Vec<Mapping>> = vec![];
     for map in mappings {
         let mut conversions: Vec<Mapping> = vec![];
@@ -23,12 +51,7 @@ pub fn seed_to_location_min(input: String) -> u32 {
         }
         maps.push(conversions);
     }
-    let mut lowest = u64::MAX;
-    for seed in seeds {
-        let location = permute_location(seed.into(), &maps);
-        lowest = std::cmp::min(lowest, location);
-    }
-    lowest.try_into().unwrap()
+    maps
 }
 
 fn permute_location(mut seed: u64, mappings: &Vec<Vec<Mapping>>) -> u64 {
@@ -83,8 +106,54 @@ humidity-to-location map:
 }
 
 #[test]
+fn test_seed_to_location_range_example() {
+    let input = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+    let value = seed_to_location_range(input.to_string());
+    assert_eq!(value, 46);
+}
+
+#[test]
 fn test_almanac_fertilizer_part_one() {
     let input = std::fs::read_to_string("./inputs/day05").unwrap();
     let actual = seed_to_location_min(input);
     assert_eq!(actual, 51752125);
+}
+
+#[test]
+fn test_almanac_fertilizer_part_two() {
+    let input = std::fs::read_to_string("./inputs/day05").unwrap();
+    let actual = seed_to_location_range(input);
+    assert_eq!(actual, 12634632);
 }
